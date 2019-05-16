d24685284d569b719c45c536a82cb335547a8f2e
dolgovdv <46597308+dolgovdv@users.noreply.github.com>
2019-05-16 12:48:39 +0300

    Update requestToServer.js

--- src/utils/requestToServer.js
+++ src/utils/requestToServer.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 
 const service = axios.create({
   // baseURL: process.env.BASE_API, // api 的 base_url
-  // baseURL: 'http://localhost:3000'
-  baseURL: 'http://194.58.100.161:3000'
+  baseURL: 'http://localhost:3000'
   // timeout: 5000 // request timeout
 })
 
